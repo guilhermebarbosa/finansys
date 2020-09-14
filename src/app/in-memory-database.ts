e340429fcca9ab1ac9a940d59bfda267ca10e2ad
Guilherme Barbosa <guilherme.barbosa90@gmail.com>
2020-09-14 11:33:01 -0300

    create db categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,16 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+
+
+export class InMemoryDatabase implements InMemoryDbService {
+  createDb(){
+    const categories = [
+      { id: 1, name: 'Lazer', description: 'lugar para lazer' },
+      { id: 2, name: 'Cinema', description: 'lugar para assistir um filme' },
+      { id: 3, name: 'Clube', description: 'lugar para jogar tenis!' },
+      { id: 4, name: 'Parque', description: 'lugar para passear' },
+      { id: 5, name: 'Casa', description: 'lugar para relaxar' },
+    ]
+
+    return { categories };
+  }
+}

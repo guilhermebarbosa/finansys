5d037eee90b8f2b9073ab17e8a627eae32037b8c
Guilherme Barbosa <guilherme.barbosa90@gmail.com>
2020-09-17 19:29:48 -0300

    adding BaseResourceFormComponent

---
+++ src/app/shared/components/base-resource-form.component.ts
@@ -0,0 +1,151 @@
+import { OnInit, Injector, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+
+import { switchMap } from "rxjs/operators";
+
+import toastr from "toastr";
+import { BaseResourceModel } from '../models/base-resource.model';
+import { BaseResourceService } from '../services/base-resource.service';
+
+export abstract class BaseResourceFormComponent<T extends BaseResourceModel> implements OnInit {
+
+  currentAction: string;
+  resourceForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submittingForm: boolean = false;
+
+  protected router: Router;
+  protected route: ActivatedRoute;
+  protected formBuilder: FormBuilder;
+
+  constructor(
+    protected injector:Injector,
+    public resource:T,
+    protected resourceService: BaseResourceService<T>,
+    protected jsonDataToResourceFn:(jsonData) => T
+  ) {
+    this.route = injector.get(ActivatedRoute);
+    this.router = injector.get(Router);
+    this.formBuilder = injector.get(FormBuilder);
+   }
+
+  ngOnInit(): void {
+    this.setCurrentAction();
+    this.buildResourceForm();
+    this.loadResource();
+  }
+
+  ngAfterContentChecked() {
+    this.setPageTitle();
+  }
+
+  // tslint:disable-next-line: typedef
+  submitForm() {
+    this.submittingForm = true;
+
+    if(this.currentAction == 'new') {
+      this.createResource();
+    }
+    else {
+      this.updateResource();
+    }
+  }
+
+  // PROTECTED METHODS
+  // tslint:disable-next-line: typedef
+  protected setCurrentAction() {
+    if(this.route.snapshot.url[0].path === 'new') {
+      this.currentAction = 'new';
+    }
+    else {
+      this.currentAction = 'edit';
+    }
+  }
+
+  // tslint:disable-next-line: typedef
+  protected loadResource(){
+    if(this.currentAction == 'edit'){
+
+      this.route.paramMap.pipe(
+        switchMap(params => this.resourceService.getById(+params.get('id')))
+      ).subscribe(
+        (resource) => {
+          this.resource = resource;
+          this.resourceForm.patchValue(resource); // bind values to form
+        },
+        (error) => alert('Ocorreu um erro no servidor, tente mais tarde')
+      );
+
+    }
+  }
+
+  // tslint:disable-next-line: typedef
+  protected setPageTitle(){
+    if(this.currentAction == 'new') {
+      this.pageTitle = this.creationPageTitle();
+    } else {
+      this.pageTitle = this.editionPageTitle();
+    }
+  }
+
+  protected creationPageTitle(): string {
+    return "Novo";
+  }
+
+  protected editionPageTitle(): string {
+    return "Edição";
+  }
+
+  // tslint:disable-next-line: typedef
+  protected createResource(){
+    const resource:T = this.jsonDataToResourceFn(this.resourceForm.value);
+
+    this.resourceService.create(resource)
+    .subscribe(
+      // tslint:disable-next-line: no-shadowed-variable
+      (resource) => this.actionsForSuccess(resource),
+      (error) => this.actionsForError(error)
+    );
+  }
+
+  // tslint:disable-next-line: typedef
+  protected updateResource(){
+    const resource:T = this.jsonDataToResourceFn(this.resourceForm.value);
+
+    this.resourceService.update(resource)
+    .subscribe(
+      // tslint:disable-next-line: no-shadowed-variable
+      (resource) => this.actionsForSuccess(resource),
+      (error) => this.actionsForError(error)
+    );
+  }
+
+  // tslint:disable-next-line: typedef
+  protected actionsForSuccess(resource: T){
+    toastr.success('A sua solicitação foi efetuada com sucesso!');
+
+    const baseComponentPath: string = this.route.snapshot.url[0].path;
+
+    this.router.navigateByUrl(baseComponentPath, {skipLocationChange: true}).then(
+      () => this.router.navigate([baseComponentPath, resource.id, 'edit'])
+    )
+  }
+
+  // tslint:disable-next-line: typedef
+  protected actionsForError(error){
+    toastr.error('Ocorreu um erro ao processar a sua solicitação!');
+
+    this.submittingForm = false;
+
+    if(error.status === 422){
+      this.serverErrorMessages = JSON.parse(error.messages).errors;
+    } else {
+      this.serverErrorMessages = ['Falha na comunicação com o servidor. Por favor, tente mais tarde.'];
+    }
+  }
+
+  protected abstract buildResourceForm(): void;
+
+}

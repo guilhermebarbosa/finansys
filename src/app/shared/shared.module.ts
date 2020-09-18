df95e2c1d94d6afe94cb9fae383d8839431bb49b
Guilherme Barbosa <guilherme.barbosa90@gmail.com>
2020-09-18 17:05:40 -0300

    adding BreadCrumbComponent && improving pages with breadcrumb

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BreadCrumbComponent
+  ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule
   ],
   exports: [
     // shared modules
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule,
+
+    // BreadCrumb
+    BreadCrumbComponent
+
   ]
 })
 export class SharedModule { }

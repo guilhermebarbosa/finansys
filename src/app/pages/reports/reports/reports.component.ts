b73ab64cabb44576383e5f3f63018ffd56cc0983
Guilherme Barbosa <guilherme.barbosa90@gmail.com>
2020-09-19 15:13:19 -0300

    adding ReportsTemplate

--- src/app/pages/reports/reports/reports.component.ts
+++ src/app/pages/reports/reports/reports.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
+  years: number[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getYears();
+  }
+
+  private getYears() {
+    const currentYear: number = (new Date()).getFullYear();
+
+    for (let index = currentYear - 5; index <= currentYear; index++) {
+      this.years.push(index);
+    }
+
+    this.years.sort((a,b) => b - a);
   }
 
 }

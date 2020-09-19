49116dd245a5746ae053281ad73f093bce13ac62
Guilherme Barbosa <guilherme.barbosa90@gmail.com>
2020-09-19 14:47:17 -0300

    adding ReportsModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { ReportsModule } from './pages/reports/reports.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule) },
   { path: 'entries', loadChildren: () => import('./pages/entries/entries.module').then(m => m.EntriesModule) },
+  { path: 'reports', loadChildren: () => import('./pages/reports/reports.module').then(m => m.ReportsModule) }
 ];
 
 @NgModule({
